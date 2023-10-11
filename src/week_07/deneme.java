f7c82694ab8fe5e17fa1098933f6f7c9c9a6f813
nialandin <nisanuraltuntas25@gmail.com>
2023-10-11 12:38:57 +0300

    week one

--- src/week_07/deneme.java
+++ src/week_07/deneme.java
@@ -3,18 +3,43 @@ package assignments.src.week_07;
 import java.util.Scanner;
 
 public class deneme {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-/*
-        int[] sayilar = {1, 2, 3, 4, 5};
-        int[] arr2 = new int[5];
-
-        for (int i = 0; i < sayilar.length; i++) {
-            arr2[i] = input.nextInt();*/
-
-
-
-
+    public static int min(int[] array) {
+        int min = array[0];
+        for (int v : array) {
+            if (v < min) {
+                min = v;
+            }
         }
+        return min;
     }
 
+    public static int gcd(int[] numbers) {
+        int gcd = 1;
+        int num = 1;
+        int min = min(numbers);
+        while (gcd < min) {
+            int count = 0;
+            for (int number : numbers) {
+                if (number % num == 0) {
+                    count++;
+                }
+                if (count == numbers.length) {
+                    gcd = num;
+                }
+            }
+            num++;
+        }
+        return gcd;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] array = new int[5];
+        System.out.print("Enter five numbers: ");
+        for (int i = 0; i < 5; i++) {
+            array[i] = input.nextInt();
+        }
+        System.out.println("The greatest common divisor is " + gcd(array));
+    }
+}
+//5 10 15 20 25
