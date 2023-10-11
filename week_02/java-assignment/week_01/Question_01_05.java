8662753b006ac025ac7d1efc93bfdc05fd3a821e
nialandin <141055146+nialandin@users.noreply.github.com>
2023-10-11 12:47:25 +0300

    Delete week_02 directory

--- week_02/java-assignment/week_01/Question_01_05.java
+++
@@ -1,8 +0,0 @@
-package assignments.src.week_01;
-
-public class Question_01_05 {
-    public static void main(String[] args) {
-        System.out.println(((9.5 * 4.5) - (2.5 * 3)) / (45.5 - 3.5));
-
-    }
-}
